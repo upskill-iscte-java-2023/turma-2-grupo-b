93932513409f572db48208edf7d5345eeae15ae7
miguel <upsk11317003@iscte-iul.pt>
2023-09-06 13:07:22 +0100

    First commit related to Webflow.

---
+++ src/main/java/plume/Main.java
@@ -0,0 +1,12 @@
+package plume;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
