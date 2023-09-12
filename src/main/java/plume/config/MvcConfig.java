7be289d580de7a96575ec6d6e291097307ddc5bb
miguel <upsk11317003@iscte-iul.pt>
2023-09-12 12:40:54 +0100

    Error with ModelAndView and SpringSecurity

--- src/main/java/plume/config/MvcConfig.java
+++ src/main/java/plume/config/MvcConfig.java
@@ -3,16 +3,26 @@ package plume.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Configuration
+@EnableWebMvc
 public class MvcConfig {
 
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setPrefix("../../WEB-INF/views/");
         resolver.setSuffix(".jsp"); // Assuming you are using JSP templates
         return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine thymeleafTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        // Configure the template engine as needed
+        return templateEngine;
     }
 }
--- src/main/java/plume/controllers/IndexController.java
+++ src/main/java/plume/controllers/IndexController.java
@@ -1,19 +1,25 @@
 package plume.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 
-@RestController
+import java.util.Map;
+
+@Controller
 @RequestMapping("/index")
 @CrossOrigin("*")
 public class IndexController {
 
     @GetMapping("/")
-    public ModelAndView helloUserController(){
-        return new ModelAndView("index");
+    public String indexController(){
+        return "index";
     }
 
 }
